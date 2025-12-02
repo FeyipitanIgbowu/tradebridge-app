c4dca94a00b289a1130dbd3451071007540de9ec
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-12-02 15:55:36 +0100

    booking services, dtos, mappers done awaiting  to be merdge

---
+++ src/main/java/org/dtos/response/BookingArtisanResponse.java
@@ -0,0 +1,8 @@
+package org.dtos.response;
+@Data
+public class BookingArtisanResponse {
+        private String id;
+        private String customerName;
+        private String artisanName;
+        private BookingStatus status;
+    }
